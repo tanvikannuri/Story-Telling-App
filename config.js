1e2087cb143a04122ad82ab64c5e95103aed7051
tanvikannuri <72737567+tanvikannuri@users.noreply.github.com>
2021-10-20 19:56:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDLv2RdKGMFn75A0VhlcaFiM4GMtKY0BDE",
+    authDomain: "story-telling-77730.firebaseapp.com",
+    projectId: "story-telling-77730",
+    storageBucket: "story-telling-77730.appspot.com",
+    messagingSenderId: "790240758831",
+    appId: "1:790240758831:web:3fc39d5f0e61edce024ada"
+  };
